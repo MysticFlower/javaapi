1291366aff88fb25fe4180e2ca67197552925d25
mysticflower <shabeena.irl@gmail.com>
2019-12-03 23:30:03 +0000

    added docker file
    updated the project with docker yml and docker file.

--- src/main/java/com/notes/notes/controller/NotesController.java
+++ src/main/java/com/notes/notes/controller/NotesController.java
@@ -9,25 +9,29 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/notes")
 public class NotesController {
     private final NotesRepository repository;
+
     @Autowired
     public NotesController(NotesRepository repository) {
         this.repository = repository;
     }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public List<Notes> getAllNotes(){
+    public List<Notes> getAllNotes() {
         return repository.findAll();
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
-    public Notes getNoteById(@PathVariable("id") ObjectId id){
+    public Notes getNoteById(@PathVariable("id") ObjectId id) {
         return repository.findBy_id(id);
     }
 
@@ -36,12 +40,14 @@ public class NotesController {
         notes.set_id(id);
         repository.save(notes);
     }
+
     @RequestMapping(value = "/", method = RequestMethod.POST)
     public Notes createNote(@Valid @RequestBody Notes notes) {
         notes.set_id(ObjectId.get());
         repository.save(notes);
         return notes;
     }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public void deleteNote(@PathVariable ObjectId id) {
         repository.delete(repository.findBy_id(id));
